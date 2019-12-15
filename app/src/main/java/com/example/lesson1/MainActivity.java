5a2053edd7bfee3af726bdcd2e160ed4f436bbaa
andrew <359747@mail.ru>
2019-12-16 00:28:47 +0700

    lesson2

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -12,47 +12,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+//    private EditText editText;
+//    private View.OnClickListener onClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            editText.setText("");
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cities);
+//        setContentView(R.layout.activity_settings);
+//        setContentView(R.layout.activity_cities_add);
 
-        final EditText editText = findViewById(R.id.editText);
-        Button button = findViewById(R.id.button);
-
-        editText.addTextChangedListener(new TextWatcher() {
-            public void afterTextChanged(Editable s) {}
-
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Resources r = getResources();
-                String user = r.getString(R.string.city, s);
-                TextView textView = findViewById(R.id.textView);
-
-                if (editText.getText().toString().isEmpty()) {
-                    textView.setText("");
-                } else {
-                    textView.setText(user);
-                }
-            }
-        });
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Resources r = getResources();
-                String city = editText.getText().toString();
-                TextView textWelcome = findViewById(R.id.city);
-
-                if (city.isEmpty()) {
-                    city = r.getString(R.string.default_city);
-                }
-
-                textWelcome.setText(city);
-                editText.setText("");
-            }
-        });
+//        Button button = findViewById(R.id.button);
+//        editText = findViewById(R.id.addCity);
+//        button.setOnClickListener(onClickListener);
     }
 }
